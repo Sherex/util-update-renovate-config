a0b195356324167fee615bbe804a366d85e9fe15
Ingar Helgesen <ingar.helgesen@t-fk.no>
2019-06-25 13:53:06 +0200

    Optimize storage

--- src/index.js
+++ src/index.js
@@ -10,22 +10,21 @@ const { logger } = require('@vtfk/logger')
   try {
     const repos = await getRepos('Sherex', 'user')
     const tempPath = await tempDir.create()
-  
-    for (let i = 0; i < 2 /*repos.length*/; i++) {
+
+    for (let i = 0; i < repos.length; i++) {
       const repo = repos[i]
       logger('info', ['index', repo.nameWithOwner, 'Processing'])
       const repoPath = await cloneRepo(repo.nameWithOwner, tempPath)
       await confRenovate(repoPath)
       await createPr(repo.id)
       logger('info', ['index', repo.nameWithOwner, 'Done'])
+      await tempDir.remove(repo.nameWithOwner)
     }
-  
+
     await tempDir.remove()
   } catch (error) {
     throw error
   }
-
-  
 })().catch(error => {
   logger('error', ['index', 'error in index', error])
   console.error(error)
